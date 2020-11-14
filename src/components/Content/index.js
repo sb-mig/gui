e0c1bf8a9b57a3c59bf2ad8b379088e6dbb660dd
marckraw <marckraw@icloud.com>
2020-11-14 16:31:16 +0100

    [chore] remove all references to OBS

---
+++ src/components/Content/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Content = () => {
+    return (
+        <div>
+            <div className="container">
+                and this is content
+            </div>
+        </div>
+    );
+};
+
+export default Content;
