880c924b226a8940c86a4c0fa04c26d1af8bd1aa
Marcin Krawczyk <marckraw@icloud.com>
2021-09-14 14:10:01 +0200

    [feat] start using preload.js script

--- public/preload.js
+++ public/preload.js
@@ -1,12 +1,20 @@
 // public/preload.js
 
+const child_process = require("child_process")
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
 const { contextBridge } = require("electron");
+
+const runCommand = (command) => {
+  return child_process.execSync(command).toString().trim()
+}
 
 // As an example, here we use the exposeInMainWorld API to expose the browsers
 // and node versions to the main window.
 // They'll be accessible at "window.versions".
 process.once("loaded", () => {
-  contextBridge.exposeInMainWorld("versions", process.versions);
+  contextBridge.exposeInMainWorld("api", {
+    versions: process.versions,
+    runCommand
+  });
 });
--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
 import React from "react";
 
+// form.addEventListener("submit", (e) => {
+//   e.preventDefault()
+//   let command = input.value
+//   let output = window.api.runCommand(command)
+//   createTerminalHistoryEntry(command, output)
+//   input.value = ""
+//   input.scrollIntoView()
+// })
+
 const App = () => {
+  console.log(window.api)
+  const output = window.api.runCommand("sb-mig --version")
+  const debugInfo = window.api.runCommand("sb-mig debug")
+  console.log(output)
   return (
     <div>
         Moja apka
