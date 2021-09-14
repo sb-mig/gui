56358178a8a9ad52c2d794b9b742a1bfbf35e4ea
Marcin Krawczyk <marckraw@icloud.com>
2021-09-14 16:35:33 +0200

    [feat] we can sync, backup, check the debug values by clicking button (for now hardcoded folder)

--- public/preload.js
+++ public/preload.js
@@ -5,8 +5,55 @@ const child_process = require("child_process")
 // It has the same sandbox as a Chrome extension.
 const { contextBridge } = require("electron");
 
-const runCommand = (command) => {
-  return child_process.execSync(command).toString().trim()
+const runCommand = (command, pathToDir) => {
+  return new Promise((resolve, reject) => {
+    child_process.exec(command, { cwd: pathToDir }, (error, stdout, stderr) => {
+      resolve({stdout, stderr, error})
+    })
+  })
+  // return child_process.execSync(command, { cwd: pathToDir }).toString().trim()
+}
+
+let runCommandAdvanced = ({command, onout, onerr, ondone, pathToDir}) => {
+  const proc = child_process.spawn(
+      command,
+      [],
+      {
+        cwd: pathToDir,
+        shell: true,
+        stdio: ["ignore", "pipe", "pipe"],
+      },
+  )
+
+  proc.stdout.on("data", (data) => {
+    console.log(data.toString())
+    return onout(data.toString())
+  })
+  proc.stderr.on("data", (data) => {
+    console.log(data.toString())
+    return onerr(data.toString())
+  })
+  proc.on("close", (code) => ondone(code))
+}
+
+let sysInfo = {
+  os: runCommand("uname -s"),
+  cpu: runCommand("uname -m"),
+  hostname: runCommand("hostname -s"),
+  ip: runCommand("ipconfig getifaddr en0"),
+}
+
+function df() {
+  let output = runCommand("df -kP")
+  let rows = output
+      .split(/[\r\n]+/)
+      .slice(1)
+  return rows.map(row => (
+      row
+          .replace(/\s+(\d+)/g, '\t$1')
+          .replace(/\s+\//g, '\t/')
+          .split(/\t/)
+  ))
 }
 
 // As an example, here we use the exposeInMainWorld API to expose the browsers
@@ -15,6 +62,9 @@ const runCommand = (command) => {
 process.once("loaded", () => {
   contextBridge.exposeInMainWorld("api", {
     versions: process.versions,
-    runCommand
+    runCommand,
+    runCommandAdvanced,
+    sysInfo,
+    df
   });
 });
--- src/App.js
+++ src/App.js
@@ -1,22 +1,72 @@
-import React from "react";
-
-// form.addEventListener("submit", (e) => {
-//   e.preventDefault()
-//   let command = input.value
-//   let output = window.api.runCommand(command)
-//   createTerminalHistoryEntry(command, output)
-//   input.value = ""
-//   input.scrollIntoView()
-// })
+import React, {useState} from "react";
 
 const App = () => {
+  const [output, setOutput] = useState('')
+  const [stdout, setStdout] = useState('')
+  const [stderr, setStderr] = useState('')
+  const [error, setError] = useState('')
+
   console.log(window.api)
-  const output = window.api.runCommand("sb-mig --version")
-  const debugInfo = window.api.runCommand("sb-mig debug")
-  console.log(output)
+
+  const run = (command, pathToDir) => {
+    let entry = {command, stdout: "", stderr: "", error: null, running: true}
+    const onout = (data) => {
+      setStdout(previous => previous + data)
+    }
+    const onerr = (data) => {
+      setStderr(previous => previous + data)
+    }
+    const ondone = (code) => {
+      setError(code !== 0)
+    }
+
+    window.api.runCommandAdvanced({command,onout,onerr,ondone, pathToDir})
+  }
+
+  const pathToDir = '/Users/marckraw/Projects/EF/ef-sbc/@documentation'
+
+  const syncSchemasAllComponentsWithExtension = () => {
+    run("sb-mig sync components --all --ext", pathToDir)
+  }
+
+  const syncSchemasAllComponentsWithExtensionWithPresets = () => {
+    run("sb-mig sync components --all --ext --presets", pathToDir)
+  }
+
+  const showDebug = () => {
+    run("sb-mig debug", pathToDir)
+  }
+
+  const showVersion = () => {
+    run("sb-mig --version", pathToDir)
+  }
+
+  const backupAllComponents = async () => {
+    run("sb-mig backup --allComponents", pathToDir)
+  }
+
+  const backupAllPresets = () => {
+    run("sb-mig backup --allPresets", pathToDir)
+  }
+
   return (
     <div>
-        Moja apka
+      <button onClick={syncSchemasAllComponentsWithExtension}>Sync schemas (all components with extension)</button>
+      <button onClick={syncSchemasAllComponentsWithExtensionWithPresets}>Sync schemas (all components with extension and presets)</button>
+      <hr/>
+      <button onClick={showDebug}>Show DEBUG - what variables sb-mig using</button>
+      <button onClick={showVersion}>Show sb-mig version</button>
+      <hr/>
+      <button onClick={backupAllComponents}>Backup All Components</button>
+      <button onClick={backupAllPresets}>Backup All Presets</button>
+      <div>
+        <h3>Messages</h3>
+        <pre>
+          {stdout}
+        </pre>
+        <h3>Errors</h3>
+        {stderr}
+      </div>
     </div>
   );
 };
